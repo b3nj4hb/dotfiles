7e376c2740511e4b8cf53232ad696c7330412367
b3nj4hb <benjalive12@gmail.com>
2023-10-30 15:17:23 -0500

    bin dir added and autolock with xss-lock and i3 autolock color

---
+++ bin/lock.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# i3 autolock color
+
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#28a8ea'
+TEXT='#28a8ea'
+WRONG='#ff0000'
+VERIFYING='#28a8ea'
+NEGRO='#131313'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$NEGRO        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--screen 1                   \
+--blur 6                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"       \
+--keylayout 1                \
